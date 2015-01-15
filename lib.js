ca895e5797ae997ae448039989437b2c1c2657fb
Synz0331 <eeakins06@gmail.com>
2015-01-15 11:43:42 -0700

    Completed Week 0 Team Func, Ind Func & Week 1 Ind Func x 4: --50 Pts

--- lib.js
+++ lib.js
@@ -5,7 +5,8 @@ lib.generateHeading1 = function(text) {
     return "<h1>" + text + "</h1>"
 }
 
-lib.generateHeading = function(level, text) {
+lib.generateHeading = function(level, text) //Team function completed
+{
     if ( level > 6 || level < 1) //Making sure that level is between 0 and 7
         //level shouldn't be less than 1 or greater than 6
     {
@@ -18,28 +19,29 @@ lib.generateHeading = function(level, text) {
     //return "not yet implemented"
 }
 
-lib.generateOrderedList = function(arrayOfText) {
-    /*
-    var ol = "<ol>"
-    arrayOfText.forEach(function(item){
-        ol += "<li>" + item + "</li>"
-    })
-    ol += "</ol>"
-    return ol
-    */
-    return "not yet implemented"
+lib.generateOrderedList = function(arrayOfText) //Week 1: Function 3 complete
+{//Ordered list header is <ol> and list items are <li>, so I cycle through the arrayOfText and add everything
+    //to a single var to be returned with all the headers included
+    var OrdList = "<ol>"
+    var endlist = "</ol>"
+    for (var i = 0; i < arrayOfText.length; ++i) 
+    {
+        OrdList = OrdList + "<li>" + arrayOfText[i] + "</li>"
+    };
+    return OrdList + endlist
+    //return "not yet implemented"
 }
 
-lib.generateUnorderedList = function(arrayOfText) {
-    /*
-    var ul = "<ul>"
-    arrayOfText.forEach(function(item){
-        ul += "<li>" + item + "</li>"
-    })
-    ul += "</ol>"
-    return ul
-    */
-    return "not yet implemented"
+lib.generateUnorderedList = function(arrayOfText) 
+{//Same as above except that it is an UnorderedList
+    var UnOrdList = "<ul>"
+    var endlist = "</ul>"
+    for (var i = 0; i < arrayOfText.length; ++i) 
+    {
+        UnOrdList = UnOrdList + "<li>" + arrayOfText[i] + "</li>"
+    };
+    return UnOrdList + endlist
+    //return "not yet implemented"
 }
 
 lib.generateTableRow = function (arrayOfText) {
@@ -58,21 +60,24 @@ lib.generateTable = function(twoDimensionalArrayOfText) {
     return "not yet implemented"
 }
 
-lib.generateHyperLink = function(url, text) {
+lib.generateHyperLink = function(url, text) //Individual #2 function completed
+{
     //In HTML, <a> </a> is similar to <h></h>....The text goes between the carats
     //Therefore, the text will refer to the address found in the first <a> carat
     return "<a href=\"" + url + "\">" + text + "</a>"
     //return "not yet implemented"
 }
 
-lib.generateFormTextField = function(name) {
-    //return "<input type=\"text\" name=\"" + name + "\">"
-    return "not yet implemented"
+lib.generateFormTextField = function(name) //week 1: Function 1 complete
+{//Form header on the outside will create the form, then the input command that a text will be inputted from the user
+    return "<form><input type=\"text\" name=\"" + name + "\"></form>"
+    //return "not yet implemented"
 }
 
-lib.generateFormTextFieldWithLabel = function(name, label) {
-    //return "<label>" + label + "</label> <input type=\"text\" name=\"" + name + "\">"
-    return "not yet implemented"
+lib.generateFormTextFieldWithLabel = function(name, label) //week 1: Function 2 complete
+{//Same as above, but now the box will be labeled for the input
+    return "<form>" + label + "<br><input type=\"text\" name=\"" + name + "\"></form>"
+    //return "not yet implemented"
 }
 
 lib.generateDropdownList = function(arrayOfValues, arrayOfText) {
